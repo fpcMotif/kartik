9120769ae8dece58c3286a5c2e72f298b8016615
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2025-04-27 23:13:53 +0530

    feat: added a background beam effect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {Inter_Tight} from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { BackgroundBeams } from "@/components/ui/background-beams"
 
 const inter = Inter_Tight({
   weight: '400',
@@ -35,24 +36,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={inter.className}
-      >
-         <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-        {children}
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <BackgroundBeams className="opacity-200" />
+          <div className="relative z-10">
+            {children}
+          </div>
         </ThemeProvider>
-      </body>
-      <script
+        <script
           src="https://script.refix.ai/script.min.js"
           type="text/javascript"
           data-refix-token="c9a48825-4062-464a-941d-c958ddf21a96"
           defer
-      ></script>
+        ></script>
+      </body>
     </html>
   );
 }
